4bc117db3471ca13aac63490ed34b0841e10ecab
CyberCrunch <robin.fischer2@gmail.com>
2017-11-11 19:49:24 +0100

    test tutorial
    uploaded mozilla tutorial webpage for heroku app

---
+++ CommandInterface/urls.py
@@ -0,0 +1,59 @@
+"""CommandInterface URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.11/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+
+from django.conf.urls import url, include
+from rest_framework import routers
+from RestAPI import views
+
+#mozilla tutorial
+from django.conf.urls import include
+from django.views.generic import RedirectView
+# Use static() to add url mapping to serve static files during development (only)
+from django.conf import settings
+from django.conf.urls.static import static
+
+'''
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+]
+'''
+
+
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UserViewSet)
+router.register(r'groups', views.GroupViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+urlpatterns = [
+    url(r'^admin/', admin.site.urls),
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    
+    #tutorial 1 lib...
+    url(r'^catalog/', include('UserControls.urls')),
+    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
+    
+    #Add Django site authentication urls (for login, logout, password management)
+    url(r'^accounts/', include('django.contrib.auth.urls'))
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ main.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 30 15:26:59 2017
+
+@author: robin
+"""
+
+"""
+AI Cloud for Dual Universe applications.
+ The purpose of this program is...
+ 
+Version: 0.01 (tech demo)
+
+Setup Webpage:
+    create ff
+"""
+import os
+import sys
+import subprocess
+
+if __name__ == "__main__":
+    print("test")
+    '''
+    os.system("start /wait cmd /c {pause}") 
+    os.system("start /wait cmd /c {TIMEOUT 10}") 
+    os.system("start /wait cmd /c {pause}") 
+    os.system("start /wait cmd /c {pause}") 
+    os.system("start /wait cmd /c {TIMEOUT 10}") 
+    os.system("start /wait cmd /c {TIMEOUT /T 10}") 
+    os.system("start /wait cmd /c {TIMEOUT /T 10 /NOBREAK}") 
+    os.system("start /wait cmd /c {NETSH Diag Ping Loopback}") 
+    os.system("start /wait cmd /c {pause}") 
+    os.system("start /wait cmd /c {pause}") 
+    os.system("start /wait cmd /c {pause}") 
+    os.system("start /wait cmd /c {pause}") 
+
+    #os.system("start /wait cmd /c {cd /d C:\\Users\\robin\\Dropbox\\UNI-SOFTWARE\\workspace_python\\AI_Cloud\\CommandInterface && python manage.py runserver}") 
+    #os.system("start /wait cmd /c {python C:\\Users\\robin\\Dropbox\\UNI-SOFTWARE\\workspace_python\\AI_Cloud\\CommandInterface\\manage.py runserver }") 
+    sts = subprocess.Popen("start /wait cmd /c {python C:\\Users\\robin\\Dropbox\\UNI-SOFTWARE\\workspace_python\\AI_Cloud\\CommandInterface\\manage.py runserver}", shell=True).wait()
+    '''    
+    #input()
